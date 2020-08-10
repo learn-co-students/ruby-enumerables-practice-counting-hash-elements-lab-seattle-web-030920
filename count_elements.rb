5e638ac8bbf42da0c2562e01407de7dcd570fe4d
SaraKhandaker <kttsara@gmail.com>
2020-08-10 09:44:17 -0700

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
+  new_hash={}
   # code goes here
+  array.each do |element|
+    if new_hash[element]
+      new_hash[element]+=1
+    else 
+      new_hash[element]=1
+    end
+  end
+  new_hash
 end
  
